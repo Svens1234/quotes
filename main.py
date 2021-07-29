6cbc749db99b919793220e1092fab1560711dc3f
Svenden123 <buhrus1@yandex.ru>
2021-07-29 21:18:59 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+string_with_triple_quotes = """string with triple quotes"""
+print(string_with_triple_quotes)
+string_with_triple_quotes1 = '''string with triple quotes'''
+print(string_with_triple_quotes1)
+string_with_triple_quotes2 = """string 
+with 
+triple 
+quotes"""
+print(string_with_triple_quotes2)
